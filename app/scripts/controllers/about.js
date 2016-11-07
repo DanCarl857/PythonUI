8f7a1b3d72d4dd87d3b5441a6b6b7d86c7c47d8a
Daniel Carlson <dancarl758@gmail.com>
2016-11-07 12:27:34 +0100

    Setting up

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name kevinApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the kevinApp
+ */
+angular.module('kevinApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
