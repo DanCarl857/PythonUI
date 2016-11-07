55b8d96a0e1e395000ac30d7c1cab871481b9b32
Daniel Carlson <dancarl758@gmail.com>
2016-11-07 13:36:37 +0100

    Finished UI given to me

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,21 +15,16 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'ui.router'
   ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .otherwise({
-        redirectTo: '/'
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/home',
+        templateUrl: 'views/home.html',
+        controller: 'homeCtrl'
       });
+
+    $urlRouterProvider.otherwise('/home');
   });
--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -8,10 +8,6 @@
  * Controller of the kevinApp
  */
 angular.module('kevinApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('homeCtrl', function () {
+
   });
