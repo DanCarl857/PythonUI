82ec11c99a6fc0298aada8b03b9bb7b7950fcef5
Daniel Carlson <dancarl758@gmail.com>
2016-11-17 16:31:09 +0100

    Added the database

--- python_ui/models.py
+++ python_ui/models.py
@@ -1,8 +1,3 @@
-from __future__ import unicode_literals
-
-from django.db import models
-
-# Create your models here.
 # This is an auto-generated Django model module.
 # You'll have to do the following manually to clean this up:
 #   * Rearrange models' order
@@ -10,6 +5,8 @@ from django.db import models
 #   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
+from __future__ import unicode_literals
+
 from django.db import models
 
 
--- python_ui/python_ui/settings.py
+++ python_ui/python_ui/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'python_ui.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'compilado',
+        'USER': 'root',
+        'PASSWORD': 'bendrisjo',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
